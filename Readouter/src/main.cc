af0f0e8f4e514730350091ecf1dca413c5c18dd8
InubashiriLix <lixinrong4002@gmail.com>
2025-08-03 10:00:28 +0800

    first draft todo: add the async function to push the string from another process
    Signed-off-by: InubashiriLix <lixinrong4002@gmail.com>

--- Readouter/src/main.cc
+++ Readouter/src/main.cc
@@ -1,6 +1,44 @@
+#include <chrono>
 #include <iostream>
+#include <queue>
+#include <string>
+#include <thread>
 
-int main() {
-    std::cout << "Hell Here" << std::endl;
+std::queue<std::string> str_queue = std::queue<std::string>();
+
+int main(int argc, char* argv[]) {
+    while (true) {
+        std::string input;
+        std::cout << "\nEnter a string > ";
+        std::getline(std::cin, input);
+
+        str_queue.push(input);
+
+        if (str_queue.empty()) {
+            continue;
+        }
+
+        std::string str_print = str_queue.front();
+        for (char& c : str_print) {
+            switch (c) {
+                case ' ': {
+                    std::cout << c << std::flush;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    break;
+                }
+                case '\n': {
+                    std::cout << c << std::flush;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    break;
+                }
+                default: {
+                    std::cout << c << std::flush;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                }
+            }
+        }
+        str_queue.pop();
+    }
+
     return 0;
 }
